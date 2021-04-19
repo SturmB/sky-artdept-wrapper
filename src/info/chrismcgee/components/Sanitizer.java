c57578aaa95ed44aa865c2e1d8e9ac40e5bb9c08
Chris McGee <chris@chrismcgee.info>
2021-04-19 16:12:01 -0400

    🎨 Cleans up formatting in the Sanitizer

--- src/info/chrismcgee/components/Sanitizer.java
+++ src/info/chrismcgee/components/Sanitizer.java
@@ -5,25 +5,23 @@ import java.util.regex.Pattern;
 
 public class Sanitizer {
 
-	private static final Pattern orderNumPattern = Pattern.compile("\\b(\\d{6,}|P\\d{5,})\\b", Pattern.CASE_INSENSITIVE);
+    private static final Pattern orderNumPattern = Pattern.compile("\\b(\\d{6,}|P\\d{5,})\\b", Pattern.CASE_INSENSITIVE);
 
-	public static boolean checkOrderNum (String orderNum)
-	{
-		Matcher orderNumMatcher = orderNumPattern.matcher(orderNum);
-		return orderNumMatcher.lookingAt();
-	}
+    public static boolean checkOrderNum(String orderNum) {
+        Matcher orderNumMatcher = orderNumPattern.matcher(orderNum);
+        return orderNumMatcher.lookingAt();
+    }
 
-	public static boolean checkInitials (String initials)
-	{
-		return initials.length() > 0;
-	}
+    public static boolean checkInitials(String initials) {
+        return initials.length() > 0;
+    }
 
-	public static boolean isValidEmail (String email) {
-		String EMAIL_PATTERN =
-			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    public static boolean isValidEmail(String email) {
+        String EMAIL_PATTERN =
+                "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
-		return email.matches(EMAIL_PATTERN);
-	}
-	
+        return email.matches(EMAIL_PATTERN);
+    }
+
 }
